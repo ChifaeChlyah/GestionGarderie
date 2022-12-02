629b49a8d49cab69f4d638c2889a5894dde62b4f
Chifae Chlyah <chifaechlyah@gmail.com>
2022-12-02 22:29:31 +0100

    first commit

---
+++ src/main/java/com/ensa/gestiongarderie/GestionGarderieApplication.java
@@ -0,0 +1,13 @@
+package com.ensa.gestiongarderie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionGarderieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionGarderieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ensa/gestiongarderie/GestionGarderieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensa.gestiongarderie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionGarderieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
