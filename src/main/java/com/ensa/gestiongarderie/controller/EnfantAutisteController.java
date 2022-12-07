95c1ed4a38ad3a887612d36b67c42906f15f900a
oussama <oussamawr6@gmail.com>
2022-12-07 18:20:52 +0100

    test11

---
+++ src/main/java/com/ensa/gestiongarderie/controller/EnfantAutisteController.java
@@ -0,0 +1,29 @@
+package com.ensa.gestiongarderie.controller;
+
+import com.ensa.gestiongarderie.entities.EnfantAutiste;
+import com.ensa.gestiongarderie.repositories.EnfantAutisteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/enfantAutiste")
+public class EnfantAutisteController {
+
+    @Autowired
+    EnfantAutisteRepository enfantAutisteRepository;
+
+
+
+    @GetMapping()
+    public List<EnfantAutiste> getAll(){
+        return enfantAutisteRepository.findAll();
+    }
+
+    @PostMapping
+    public void createEtuAutiste(@RequestBody EnfantAutiste enfantAutiste){
+        enfantAutisteRepository.save(enfantAutiste);
+    }
+}
+
