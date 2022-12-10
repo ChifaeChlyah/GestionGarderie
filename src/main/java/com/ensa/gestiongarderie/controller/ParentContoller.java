89528398627b380bbb9b842f497580eb1f7f1f2c
Chifae Chlyah <chifaechlyah@gmail.com>
2022-12-11 00:51:56 +0100

    connection

--- src/main/java/com/ensa/gestiongarderie/controller/ParentContoller.java
+++ src/main/java/com/ensa/gestiongarderie/controller/ParentContoller.java
@@ -1,10 +1,12 @@
 package com.ensa.gestiongarderie.controller;
 
 
+import com.ensa.gestiongarderie.entities.Administrateur;
 import com.ensa.gestiongarderie.entities.Enfant;
 import com.ensa.gestiongarderie.entities.IEnfant;
 import com.ensa.gestiongarderie.entities.Parent;
 import com.ensa.gestiongarderie.factory_service.EnfantFactory;
+import com.ensa.gestiongarderie.mapping.Connection;
 import com.ensa.gestiongarderie.repositories.EnfantRepository;
 import com.ensa.gestiongarderie.repositories.ParentRepository;
 import com.ensa.gestiongarderie.services.PaymentStrategy;
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/parent")
 public class ParentContoller {
@@ -67,6 +69,16 @@ public class ParentContoller {
         double prix=enfant.cout();
         if(paymentStrategy.payer(prix))
             parent.setStatutPayement(true);
+    }
+    @PostMapping("/connection")
+    public boolean verifierCompte(@RequestBody Connection connection){
+        Parent a=parentRepository.findByEmail(connection.getEmail());
+        if(a!=null){
+            if(a.getMotDePasse().equals(connection.getPassword())){
+                return  true;
+            }
+        }
+        return  false ;
     }
 //@Autowired
 //    EnfantRepository enfantRepository;
--- src/main/java/com/ensa/gestiongarderie/repositories/ParentRepository.java
+++ src/main/java/com/ensa/gestiongarderie/repositories/ParentRepository.java
@@ -1,5 +1,6 @@
 package com.ensa.gestiongarderie.repositories;
 
+import com.ensa.gestiongarderie.entities.Administrateur;
 import com.ensa.gestiongarderie.entities.Parent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -8,4 +9,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("*")
 @RepositoryRestResource
 public interface ParentRepository extends JpaRepository<Parent,Long> {
+    public Parent findByEmail(String email);
 }
